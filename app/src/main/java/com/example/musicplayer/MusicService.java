17d973eca4f810bc2238b90ae0427e94e018d6ab
an27k <an27kr09@gmail.com>
2022-05-08 17:43:40 +0530

    part 23

--- app/src/main/java/com/example/musicplayer/MusicService.java
+++ app/src/main/java/com/example/musicplayer/MusicService.java
@@ -11,6 +11,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.MediaMetadataRetriever;
@@ -35,6 +36,10 @@ public class MusicService extends Service implements MediaPlayer.OnCompletionLis
     int position = -1;
     ActionPlaying actionPlaying;
     MediaSessionCompat mediaSessionCompat;
+    public static final String MUSIC_LAST_PLAYED = "LAST_PLAYED";
+    public static final String MUSIC_FILE = "STORED_MUSIC";
+    public static final String ARTIST_NAME = "ARTIST NAME";
+    public static final String SONG_NAME = "SONG NAME";
 
     @Override
     public void onCreate() {
@@ -132,6 +137,11 @@ public class MusicService extends Service implements MediaPlayer.OnCompletionLis
     void createMediaPlayer(int positionInner){
         position=positionInner;
         uri = Uri.parse(musicFiles.get(position).getPath());
+        SharedPreferences.Editor editor = getSharedPreferences(MUSIC_LAST_PLAYED, MODE_PRIVATE).edit();
+        editor.putString(MUSIC_FILE, uri.toString());
+        editor.putString(ARTIST_NAME, musicFiles.get(position).getArtist());
+        editor.putString(SONG_NAME, musicFiles.get(position).getTitle());
+        editor.apply();
         mediaPlayer= MediaPlayer.create(getBaseContext(), uri);
     }
     void pause(){
--- app/src/main/java/com/example/musicplayer/NowPlayingFragmentBottom.java
+++ app/src/main/java/com/example/musicplayer/NowPlayingFragmentBottom.java
@@ -1,5 +1,11 @@
 package com.example.musicplayer;
 
+import static com.example.musicplayer.MainActivity.ARTIST_TO_FRAG;
+import static com.example.musicplayer.MainActivity.PATH_TO_FRAG;
+import static com.example.musicplayer.MainActivity.SHOW_MINI_PLAYER;
+import static com.example.musicplayer.MainActivity.SONG_NAME_TO_FRAG;
+
+import android.media.MediaMetadataRetriever;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,7 +15,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class NowPlayingFragmentBottom extends Fragment {
@@ -33,7 +41,44 @@ public class NowPlayingFragmentBottom extends Fragment {
         albumArt=view.findViewById(R.id.bottom_album_art);
         nextBtn=view.findViewById(R.id.skip_next_bottom);
         playPauseBtn=view.findViewById(R.id.play_pause_miniPlayer);
-
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Next", Toast.LENGTH_SHORT).show();
+            }
+        });
+        playPauseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "PlayPause", Toast.LENGTH_SHORT).show();
+            }
+        });
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(SHOW_MINI_PLAYER) {
+            if (PATH_TO_FRAG != null) {
+                byte[] art = getAlbumArt(PATH_TO_FRAG);
+                if(art != null){
+                    Glide.with(getContext()).load(art).into(albumArt);
+                }
+                else {
+                    Glide.with(getContext()).load(R.drawable.defimage).into(albumArt);
+                }
+                songName.setText(SONG_NAME_TO_FRAG);
+                artist.setText(ARTIST_TO_FRAG);
+            }
+        }
+    }
+
+    private byte[] getAlbumArt(String uri){
+        MediaMetadataRetriever retriever=new MediaMetadataRetriever();
+        retriever.setDataSource(uri);
+        byte[] art=retriever.getEmbeddedPicture();
+        retriever.release();
+        return art;
     }
 }
